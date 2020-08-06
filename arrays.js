3832cb6e81ad802aab0e812f073480d06519e31d
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-06 23:14:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred frand", "kitkat", "skittles"];
+function addElementToBeginningOfArray() {
+  chocolateBars = [...chocolateBars, "Element"];
+return chocolateBars;
+}
+
+
+function destructivelyAddElementToBeginningOfArray() {
+  chocolateBars.unshift("Element");
+}
