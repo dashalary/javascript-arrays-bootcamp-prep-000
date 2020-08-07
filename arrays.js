e5ae0d337d81114179acdd121804c1a2b3350364
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-07 00:48:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,6 @@ var candyString = "foo"
 
 function addElementToBeginningOfArray(chocolateBars, candyString) {
   return [candyString, ...chocolateBars];
-
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
