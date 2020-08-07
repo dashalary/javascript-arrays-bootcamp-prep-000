0eecc1f61d2c915317b82ed86b1b8aacd70c98a3
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-07 00:49:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //var candyString = "foo"
 
-function addElementToBeginningOfArray(chocolateBars, candyString) {
+function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
 
